b2039c0fb019c46c245690f0d18c569d5e30e6eb
nickyohn <32683835+nickyohn@users.noreply.github.com>
2021-01-05 13:51:46 -0500

    Update wolf-goat-cabbage_problem.py

--- wolf-goat-cabbage_problem.py
+++ wolf-goat-cabbage_problem.py
@@ -1,3 +1,4 @@
+# list of characters (all start on west bank)
 list = ['man', 'goat', 'wolf', 'cabbage']
 path = []
 
@@ -15,6 +16,22 @@ def eats(x, y):
 # defines pair of characters who can safely cross the river
 def safe_pair(x, y):
     if eats(x, y) or eats(y, x):
+        return False
+    else:
+        return True
+
+# returns state of each character
+def state_of(character, state):
+    try:
+        return state[character]
+
+# returns true/false for different safe states
+def safe_state(state):
+    if state_of('man', state) == state_of('goat', state):
+        return True
+    elif state_of('goat', state) == state_of('wolf', state):
+        return False
+    elif state_of('goat', state) == state_of('cabbage', state):
         return False
     else:
         return True
